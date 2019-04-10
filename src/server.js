d06092d78767a281adb41f5fc77b06fe13b0f78c
Gabriel de Oliveira Gonçalves <ggabriel_667@yahoo.com.br>
2019-04-10 08:42:00 -0300

    alteracao no server

--- src/server.js
+++ src/server.js
@@ -10,13 +10,11 @@ app.use(cors());
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-io.on("connection"),
-  socket => {
-    socket.on("connectRoom"),
-      box => {
-        socket.join(box);
-      };
-  };
+io.on("connection", socket => {
+  socket.on("connectRoom", box => {
+    socket.join(box);
+  });
+});
 
 mongoose.connect(
   "mongodb+srv://mydb:harvest@cluster0-wnv0z.mongodb.net/omnistack?retryWrites=true",
